3504b08cf94013a66ff04f04f786e7bd88937bbe
michaelqmaguire <67400297+michaelqmaguire@users.noreply.github.com>
2021-06-10 11:19:50 -0400

    modified x axis text to make year-quarter visible on plots

--- 04_make-plots.R
+++ 04_make-plots.R
@@ -17,124 +17,130 @@ drugsAggState %>%
   filter(suppression == "F") %>%
   mutate(yearQuarter = paste0(year, "-", quarter)) %>%
   ggplot() +
-  geom_col(
-    mapping = aes(
-      x    = yearQuarter,
-      y    = totalRX,
-      fill = yearQuarter
-    )
-  ) +
-  scale_fill_viridis_d() +
-  theme_ipsum_rc(grid = "XY") +
-  theme(
-    legend.position = "none"
-  ) +
-  ggtitle("Total Prescriptions by Year and Quarter") +
-  scale_y_continuous(labels = scales::comma)
+    geom_col(
+      mapping = aes(
+        x    = yearQuarter,
+        y    = totalRX,
+        fill = yearQuarter
+      )
+    ) +
+    scale_fill_viridis_d() +
+    theme_ipsum_rc(grid = "XY") +
+    theme(
+      legend.position = "none"
+    ) +
+    ggtitle("Total Prescriptions by Year and Quarter") +
+    scale_y_continuous(labels = scales::comma)
 
-plotDrugsAggState <- 
+
 drugsAggState %>%
   filter(suppression == "F") %>%
   mutate(yearQuarter = paste0(year, "-", quarter)) %>%
   ggplot() +
-  geom_col(
-    mapping = aes(
-      x    = yearQuarter,
-      y    = totalRX,
-      fill = state
-    )
-  ) +
-  facet_wrap(~ state) +
-  scale_fill_viridis_d() +
-  theme_ipsum_rc(grid = "XY") +
-  theme(
-    legend.position = "none",
-    axis.text.x = element_text(angle = 90, size = 9, vjust = 0.5)
-  ) +
-  ggtitle("Total Prescriptions by State, Year, and Quarter") +
-  scale_y_continuous(labels = scales::comma)
+    geom_col(
+      mapping = aes(
+        x    = yearQuarter,
+        y    = totalRX,
+        fill = state
+      )
+    ) +
+    facet_wrap(~ state) +
+    scale_fill_viridis_d() +
+    theme_ipsum_rc(grid = "XY") +
+    theme(
+      legend.position = "none",
+      axis.text.x = element_text(angle = 90, size = 9, vjust = 0.5)
+    ) +
+    ggtitle("Total Prescriptions by State, Year, and Quarter") +
+    scale_y_continuous(labels = scales::comma)
+
 
 drugsAggStateGeneric %>%
   filter(suppression == "F") %>%
   mutate(yearQuarter = paste0(year, "-", quarter)) %>%
   ggplot() +
-  geom_col(
-    mapping = aes(
-      x    = yearQuarter,
-      y    = totalRX,
-      fill = gennme
-    )
-  ) +
-  labs(
-    fill = "Generic Name"
-  ) +
-  scale_fill_viridis_d() +
-  theme_ipsum_rc(grid = "XY") +
-  ggtitle("Total Prescriptions by Year, Quarter, and Generic Name") +
-  scale_y_continuous(labels = scales::comma)
+    geom_col(
+      mapping = aes(
+        x    = yearQuarter,
+        y    = totalRX,
+        fill = gennme
+      )
+    ) +
+    labs(
+      fill = "Generic Name"
+    ) +
+    scale_fill_viridis_d() +
+    theme_ipsum_rc(grid = "XY") +
+    ggtitle("Total Prescriptions by Year, Quarter, and Generic Name") +
+    scale_y_continuous(labels = scales::comma)
 
 drugsAggStateGeneric %>%
   filter(suppression == "F") %>%
   mutate(yearQuarter = paste0(year, "-", quarter)) %>%
   ggplot() +
-  geom_col(
-    mapping = aes(
-      x    = yearQuarter,
-      y    = totalRX,
-      fill = gennme
-    )
-  ) +
-  facet_wrap(~ state) +
-  labs(
-    fill = "Generic Name"
-  ) +
-  scale_fill_viridis_d() +
-  theme_ipsum_rc(grid = "XY") +
-  ggtitle("Total Prescriptions by State, Year, Quarter, and Generic Name") +
-  scale_y_continuous(labels = scales::comma)
+    geom_col(
+      mapping = aes(
+        x    = yearQuarter,
+        y    = totalRX,
+        fill = gennme
+      )
+    ) +
+    facet_wrap(~ state) +
+    labs(
+      fill = "Generic Name"
+    ) +
+    scale_fill_viridis_d() +
+    theme_ipsum_rc(grid = "XY") +
+    theme(
+      axis.text.x = element_text(angle = 90, size = 8, vjust = 0.5)
+    ) +
+    ggtitle("Total Prescriptions by State, Year, Quarter, and Generic Name") +
+    scale_y_continuous(labels = scales::comma)
 
 drugsAggStateProdnme %>%
   filter(suppression == "F") %>%
   mutate(
     yearQuarter = paste0(year, "-", quarter),
-    prodnme = stringr::str_to_title(prodnme)) %>%
+    prodnme = stringr::str_to_title(prodnme)
+  ) %>%
   ggplot() +
-  geom_col(
-    mapping = aes(
-      x    = yearQuarter,
-      y    = totalRX,
-      fill = prodnme
-    )
-  ) +
-  labs(
-    fill = "Brand Name"
-  ) +
-  scale_fill_viridis_d() +
-  theme_ipsum_rc(grid = "XY") +
-  ggtitle("Total Prescriptions by Year, Quarter, and Brand Name") +
-  scale_y_continuous(labels = scales::comma)
+    geom_col(
+      mapping = aes(
+        x    = yearQuarter,
+        y    = totalRX,
+        fill = prodnme
+      )
+    ) +
+    labs(
+      fill = "Brand Name"
+    ) +
+    scale_fill_viridis_d() +
+    theme_ipsum_rc(grid = "XY") +
+    ggtitle("Total Prescriptions by Year, Quarter, and Brand Name") +
+    scale_y_continuous(labels = scales::comma)
 
 drugsAggStateProdnme %>%
   filter(suppression == "F") %>%
   mutate(
     yearQuarter = paste0(year, "-", quarter),
-    prodnme = stringr::str_to_title(prodnme)) %>%
+    prodnme = stringr::str_to_title(prodnme)
+  ) %>%
   ggplot() +
-  geom_col(
-    mapping = aes(
-      x    = yearQuarter,
-      y    = totalRX,
-      fill = prodnme
-    )
-  ) +
-  facet_wrap(~ state) +
-  labs(
-    fill = "Brand Name"
-  ) +
-  scale_fill_viridis_d() +
-  theme_ipsum_rc(grid = "XY") +
-  theme(
-    axis.text.x = element_text(angle = 90, size = 9, vjust = 0.5)
-  ) +
-  ggtitle("Total Prescriptions by State, Year, Quarter, and Brand Name") +
-  scale_y_continuous(labels = scales::comma)
+    geom_col(
+      mapping = aes(
+        x    = yearQuarter,
+        y    = totalRX,
+        fill = prodnme
+      )
+    ) +
+    facet_wrap(~ state) +
+    labs(
+      fill = "Brand Name"
+    ) +
+    scale_fill_viridis_d() +
+    theme_ipsum_rc(grid = "XY") +
+    theme(
+      axis.text.x = element_text(angle = 90, size = 7, vjust = 0.5)
+    ) +
+    ggtitle("Total Prescriptions by State, Year, Quarter, and Brand Name") +
+    scale_y_continuous(labels = scales::comma)
